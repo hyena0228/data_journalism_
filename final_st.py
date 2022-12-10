b2ae7d2f2298cb9b25fe57fe246ecb88e8b66df2
hyena0228 <hyena0228@snu.ac.kr>
2022-12-11 02:23:54 +0900

    Update final_st.py

--- final_st.py
+++ final_st.py
@@ -109,10 +109,11 @@ st.table(harm)
 #올 여름 서울과 포항 호우 관련 데이터 확인
 rain_2022 = load_excel("2022 강수.xlsx")
 
-rain_2022.index = ["2022년 일일강수량 합"]
+
 rain_2022 = pd.DataFrame({"서울": [1770], "포항": [1055]})
+rain_2022.index = ["2022년 일일강수량 합"]
 #rain_2022.columns = []
-st.bar_chart(rain_2022, width=800)
+st.bar_chart(rain_2022, width=500)
 
 
 
